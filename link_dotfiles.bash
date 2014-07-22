b2b9aeaab9ffab662dd60c53230c1555c186b9dc
Ryan Manly <ryan.manly@gmail.com>
2014-07-21 21:52:40 -0500

    adding error checking etc.

--- link_dotfiles.bash
+++ link_dotfiles.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-for item in ./dotfiles/*; do
+shopt -s nullglob
+
+if [[ -z $1 ]]; then
+    echo "Must supply path to dotfiles repo."
+    exit 1
+fi
+
+for item in "$1"; do
     ln -s "${item}" "$HOME/.${foo##*/}"
 done
