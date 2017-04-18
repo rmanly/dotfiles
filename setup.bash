c9710267d6c6c9887e5c6fe45e453bf47f9dff21
Ryan Manly <ryan.manly@gmail.com>
2017-04-18 13:48:29 -0500

    whoops

--- setup.bash
+++ setup.bash
@@ -4,5 +4,5 @@ git clone https://github.com/rmanly/dotfiles.git $HOME/src/dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
-ln -s $HOME/src/dotfiles/bash_profile $HOME/.bashrc
+ln -s $HOME/src/dotfiles/bash_profile $HOME/.bash_profile
 ln -s $HOME/src/dotfiles/vimrc $HOME/.vimrc
