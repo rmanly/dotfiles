64ce4c91c5cbdc8a19ab6744db1c53e67f848828
Ryan Manly <ryan.manly@gmail.com>
2017-04-18 14:13:28 -0500

    better

--- setup.bash
+++ setup.bash
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/rmanly/dotfiles.git $HOME/src/dotfiles
+if [[ -d $HOME/src/dotfiles/.git ]]; then
+    cd $HOME/src/dotfiles/.git
+    git fetch --all
+    git reset --hard origin/master
+else
+    git clone https://github.com/rmanly/dotfiles.git $HOME/src/dotfiles
+fi
+
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
-ln -s $HOME/src/dotfiles/bash_profile $HOME/.bash_profile
-ln -s $HOME/src/dotfiles/vimrc $HOME/.vimrc
+[[ -e $HOME/.bashrc ]] && /bin/rm $HOME/.bashrc
+[[ -e $HOME/.bash_profile ]] && /bin/rm $HOME/.bash_profile
+[[ -e $HOME/.vimrc ]] && /bin/rm $HOME/.vimrc
+
+/bin/ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
+/bin/ln -s $HOME/src/dotfiles/bash_profile $HOME/.bash_profile
+/bin/ln -s $HOME/src/dotfiles/vimrc $HOME/.vimrc
